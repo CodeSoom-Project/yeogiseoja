adefe1b033a1757398952a6fee30f5f12c35a23d
Lee Byoung Seong <dev.bslee@gmail.com>
2022-05-19 20:27:06 +0900

    #1 - [숙소 도메인 생성] City,State Entity생성

---
+++ src/main/java/com/yeogiseoja/domain/accommodation/address/City.java
@@ -0,0 +1,26 @@
+package com.yeogiseoja.domain.accommodation.address;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class City {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "city_id")
+    private Long id;
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "state_id")
+    private State state;
+
+    public City(Long id, String name, State state) {
+        this.id = id;
+        this.name = name;
+        this.state = state;
+    }
+}
---
+++ src/main/java/com/yeogiseoja/domain/accommodation/address/State.java
@@ -0,0 +1,21 @@
+package com.yeogiseoja.domain.accommodation.address;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class State {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "state_id")
+    private Long id;
+    private String name;
+
+    public State(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
